250c68d7f62d35097719a034c662fc6413e38c1f
Thorsten Klöhn <thorstenkloehn@gmail.com>
2021-07-11 17:59:49 +0200

    Test

---
+++ C#/01_Lektion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01_Lektion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ganzezahlen
+            byte wbyte = 200;
+            System.Console.WriteLine(wbyte);
+            short wshort = 3000;
+            System.Console.WriteLine(wshort);
+            //Kommazahlen
+            float wfloat = 3.4f;
+            double wdouble = 3.14;
+            
+            System.Console.WriteLine(wfloat);
+            System.Console.WriteLine(wdouble);
+        }
+    }
+}
