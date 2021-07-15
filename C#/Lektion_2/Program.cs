64c18c4d31cc64036c283e5f092e3896c3f686fe
Thorsten Klöhn <thorstenkloehn@gmail.com>
2021-07-15 19:37:03 +0200

    Test

---
+++ C#/Lektion_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lektion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const  int zahl = 100;
+            Console.WriteLine(zahl);
+        }
+    }
+}
